c83a8fe3dda1b1fedbd3ee5d8d13cc402fd196b2
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-14 20:39:59 +0900

    feat: use firestore

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,35 +1,47 @@
 import { NotFoundException } from '@nestjs/common'
 import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { admin } from 'src/modules/admin'
 import { User } from 'src/user/user.model'
 import { Post } from './post.model'
-
-const dummyUsers = [
-  { id: '1', name: 'demo-name' },
-  { id: '2', name: 'demo-name' },
-]
-
-const dummyPosts = [
-  { id: '1', title: 'demo-title', body: 'body1', userId: '1' },
-  { id: '2', title: 'demo-title', body: 'body2', userId: '1' },
-]
 
 @Resolver(() => Post)
 export class PostResolver {
   @Query(() => Post)
   async post(@Args('id') id: string): Promise<Post> {
-    const post = dummyPosts.find((post) => post.id === id)
+    console.log('PostResolver#post')
+
+    const post = await admin
+      .firestore()
+      .collection('posts')
+      .doc(id)
+      .get()
+      .then((snap) => snap.data())
     if (post === undefined) throw new NotFoundException()
-    return post
+    return { id, ...post } as Post
   }
 
   @Query(() => [Post])
   async posts(): Promise<Post[]> {
-    return dummyPosts
+    console.log('PostResolver#posts')
+
+    return (await admin
+      .firestore()
+      .collection('posts')
+      .get()
+      .then((snap) =>
+        snap.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
+      )) as Post[]
   }
 
   @ResolveField(() => User)
   async user(@Parent() post: Post) {
-    const user = dummyUsers.find((u) => u.id === post.userId)
+    console.log('PostResolver#user')
+    const user = admin
+      .firestore()
+      .collection('users')
+      .doc(post.userId)
+      .get()
+      .then((snap) => ({ id: snap.id, ...snap.data() }))
     if (user === undefined) throw new NotFoundException()
     return user
   }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,5 +1,21 @@
-import { Resolver } from '@nestjs/graphql'
+import { NotFoundException } from '@nestjs/common'
+import { Args, Resolver, Query } from '@nestjs/graphql'
+import { admin } from 'src/modules/admin'
 import { User } from './user.model'
 
 @Resolver(() => User)
-export class UserResolver {}
+export class UserResolver {
+  @Query(() => User)
+  async user(@Args('id') id: string): Promise<User> {
+    console.log('UserResolver#user')
+
+    const user = await admin
+      .firestore()
+      .collection('users')
+      .doc(id)
+      .get()
+      .then((snap) => snap.data())
+    if (user === undefined) throw new NotFoundException()
+    return { id, ...user } as User
+  }
+}
