2cec0cef4c47ca2d38b13cb7034376e365587df6
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-15 23:32:42 +0900

    chore

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-	import { ApolloClient, InMemoryCache } from '@apollo/client';
-	import { GetPost, GetPostQuery } from 'src/generated/graphql';
-	import { onMount } from 'svelte';
+  import { ApolloClient, InMemoryCache } from '@apollo/client'
+  import { GetPost, GetPostQuery } from 'src/generated/graphql'
+  import { onMount } from 'svelte'
 
-	const client = new ApolloClient({
-		uri: 'http"//localhost:3000/graphql',
-		cache: new InMemoryCache()
-	});
+  const client = new ApolloClient({
+    uri: 'http"//localhost:3000/graphql',
+    cache: new InMemoryCache(),
+  })
 
-	let post: GetPostQuery['post'];
-	onMount(async () => {
-		const { data, errors } = await client.query<GetPostQuery>({ query: GetPost });
-		if (errors) console.error(errors);
-		post = data.post;
-	});
+  let post: GetPostQuery['post']
+
+  onMount(async () => {
+    const { data, errors } = await client.query<GetPostQuery>({ query: GetPost })
+    if (errors) console.error(errors)
+    post = data.post
+  })
 </script>
 
 <svelte:head>
-	<title>Hello world!</title>
+  <title>Hello world!</title>
 </svelte:head>
 
 <main>
-	<h1>{post.title}</h1>
-	<p>著者：{post.user.name}</p>
+  <h1>{post.title}</h1>
+  <p>著者：{post.user.name}</p>
 
-	<p>{post.body}</p>
+  <p>{post.body}</p>
 </main>
