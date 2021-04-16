acab602a9f625c0abfdde6ebc04a177a4f0173d9
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-16 12:50:37 +0900

    fix: endpoint url

--- web/src/lib/modules/urql.ts
+++ web/src/lib/modules/urql.ts
@@ -3,6 +3,6 @@ import { createClient } from '@urql/core'
 export const client = createClient({
   url:
     process.env.NODE_ENV === 'production'
-      ? 'https://nest-firestore-graphql-dot-kiyo-dev-944.an.r.appspot.com/'
+      ? 'https://nest-firestore-graphql-dot-kiyo-dev-944.an.r.appspot.com/graphql'
       : 'http://localhost:3000/graphql',
 })
